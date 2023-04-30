f73d0d06b8454e4bb5c9ded1d7b6198efc4b51ed
Lionel Père <lionelpere@gmail.com>
2023-04-30 10:32:29 +0200

    Include new Wiki flag in Docker container

--- src/docker-script.sh
+++ src/docker-script.sh
@@ -2,8 +2,18 @@
 RETENTION_DAYS="${RETENTION_IN_DAYS:-7}"
 
 if [[ -z "${DRY_RUN}" ]]; then
+    if [[ -z "${WIKI}" ]]; then
     ./backup-devops.sh -p $DEVOPS_PAT -o $DEVOPS_ORG_URL -d /data -r $RETENTION_DAYS
+    else
+     echo "== INCLUDE WIKI"
+    ./backup-devops.sh -p $DEVOPS_PAT -o $DEVOPS_ORG_URL -d /data -r $RETENTION_DAYS -w
+    fi
 else
     echo "== DRY RUN EXECUTION"
+    if [[ -z "${WIKI}" ]]; then
     ./backup-devops.sh -p $DEVOPS_PAT -o $DEVOPS_ORG_URL -d /data -r $RETENTION_DAYS --dryrun true
+    else
+    echo "== INCLUDE WIKI"
+    ./backup-devops.sh -p $DEVOPS_PAT -o $DEVOPS_ORG_URL -d /data -r $RETENTION_DAYS --dryrun true -w
+    fi
 fi
